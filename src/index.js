5be6d7851c08774927b09af73bd59ad700c1a5b2
Ray Lopez <raylopez509@gmail.com>
2025-03-26 15:27:57 -0700

    created Ship class Ship class has a length, times hit, and has boolean if it's sunk

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log("test");
+
+export class Ship {
+  constructor(length, timesHit, sunk) {
+    this.length = length;
+    this.timesHit = timesHit;
+    this.sunk = sunk;
+  }
+  hit() {
+    this.timesHit++;
+  }
+  isSunk() {
+    if(this.timesHit >= this.length) {
+      this.sunk = true;
+    }
+    else {
+      this.sunk = false;
+    }
+    return this.sunk;
+  }
+}
