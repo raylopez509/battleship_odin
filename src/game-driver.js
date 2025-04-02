4a627cc0d8d20fafb9636a92e38969224741267b
Ray Lopez <raylopez509@gmail.com>
2025-04-02 08:51:11 -0700

    new driver to setup the battleship games

---
+++ src/game-driver.js
@@ -0,0 +1,12 @@
+import { Ship, Gameboard, Player } from './index.js';
+
+let player1 = Player(false);
+let player2 = Player(false);
+
+function setupNewGame() {
+  const player1 = Player(false);
+  player1.gameBoard
+  const player2 = Player(false);
+}
+
+// function displayGameBoard()
